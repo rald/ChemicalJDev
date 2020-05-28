a066d4f03579004e13f2cf7752f359f064f0b137
rald <thecoconutnutisagiantnut@gmail.com>
2020-05-28 12:29:30 +0800

    update

---
+++ ChemicalJ-04/Button.js
@@ -0,0 +1,42 @@
+function Button(x,y,w,h) {
+	this.x=x;
+	this.y=y;
+	this.w=w;
+	this.h=h;
+	this.state=Button.UP;
+
+	this.draw=function() {
+		switch(this.state) {
+			case Button.UP:
+				setFillStyle("#00000080");
+				setStrokeStyle("#FFFFFF80");
+				fillRect(this.x,this.y,this.w,this.h);
+				drawRect(this.x,this.y,this.w,this.h);
+				break;
+			case Button.DOWN:
+				setFillStyle("#FFFFFF80");
+				setStrokeStyle("#00000080");
+				fillRect(this.x,this.y,this.w,this.h);
+				drawRect(this.x,this.y,this.w,this.h);
+				break;
+		}
+	}
+
+	this.handleEvents=function(touches) {
+		this.state=Button.UP;
+		if(touches!=null) {
+			for(var i=touches.length-1;i>=0;i--) {
+			  var touch=touches[i];
+			  if(inrect(touch.clientX,touch.clientY,this.x,this.y,this.w,this.h)) {
+					this.state=Button.DOWN;
+					return true;
+			  }
+			}
+		}
+		return false;
+	}
+
+}
+
+Button.UP=0;
+Button.DOWN=1;
