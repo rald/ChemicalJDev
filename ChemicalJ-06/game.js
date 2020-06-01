759ce576a8b4d461ba7d42923660f16c4e237d6b
rald <thecoconutnutisagiantnut@gmail.com>
2020-06-02 07:56:32 +0800

    update

--- ChemicalJ-06/game.js
+++ ChemicalJ-06/game.js
@@ -1,5 +1,13 @@
+function rnd(n) {
+	return Math.floor(Math.random()*n);
+}
+
+function RGB(r,g,b) {
+	return "rgb("+r+","+g+","+b+")";
+}
+
 function setPixel(x,y,s) {
-  ctx.fillRect(x*s, y*s, s, s);
+  ctx.fillRect(x*s,y*s,s,s);
 }
 
 function printChar(f,w,h,c,x,y,s,fg,bg) {
@@ -21,6 +29,7 @@ function printChar(f,w,h,c,x,y,s,fg,bg) {
 function print(f,w,h,t,x,y,s,fg,bg) {
 	for(var i=0;i<t.length;i++) {
 		var c=t.charAt(i);
+
 		if(c=='\n') {
 			y+=h;
 		} else if(c=='\r') {
@@ -36,8 +45,8 @@ function print(f,w,h,t,x,y,s,fg,bg) {
 			x=0;
 			y+=h;
 		}
-
 	}
 }
 
-print(font,8,8,"Hello World\rHi!",0,0,2,"#FFFFFF","#000000");
+print(font,8,8,"The quick brown fox jumps over the lazy dog.",0,0,2,"#FFFFFF","#000000");
+print(font,8,8,"THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.",0,16,2,"#FFFFFF","#000000");
